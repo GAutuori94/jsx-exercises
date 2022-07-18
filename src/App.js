5910e29f8af2107ed7620f39122674142a54110c
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-07-18 10:42:09 +0200

    feat: HelloWorld component added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HelloWorld } from './Jsx-01';
+
+export class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <HelloWorld />
+            </div>
+        )
+    }
+}
+
+export default App
